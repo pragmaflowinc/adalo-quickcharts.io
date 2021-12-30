f24e5863658ca02fc10415748b9f74d24c139d6b
Mitch Schwartz <mitch@pragmaflow.com>
2021-12-30 10:14:17 -0500

    some things that didn't do anything

--- src/components/Quickcharts/quickChartsIo.js
+++ src/components/Quickcharts/quickChartsIo.js
@@ -33,7 +33,9 @@ class Quickcharts extends Component {
 		let c = {type: "bar", data: {}}
 		c.data.labels = this.props.listItems.map(item => item["xValues"])
 		const aggregatedData = this.props.listItems.reduce((acc, item) => {
+			
 		
+
 			if (!acc[item.xValues]){
 				acc[item.xValues] = item.yValues
 			}
@@ -43,9 +45,7 @@ class Quickcharts extends Component {
 
 			return acc
 
-			},
-			
-			{}
+			},{}
 		)
 		
 		console.log(aggregatedData)
@@ -56,15 +56,14 @@ class Quickcharts extends Component {
 			data: Object.keys(aggregatedData).map(key => aggregatedData[key])
 		}]
 		
-		let url = () => {
-			if(this.props.onUrl){
-				this.props.onUrl(
-					this.props.editor ? 
-						`https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}` 
-					
-						: `https://quickchart.io/chart?c=${encodeURIComponent(JSON.stringify(c))}`
-				)
-			}
+		if(this.props.onUrl){
+			this.props.onUrl(
+				this.props.editor 
+				
+				? `https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}`
+
+				: `https://quickchart.io/chart?c=${JSON.stringify(c)}`
+			)
 		}
 
 		if(this.props.editor){
@@ -74,7 +73,7 @@ class Quickcharts extends Component {
 				<Image
 				  style={styles.chart}
 				  source={{
-					uri: `${Url}`,
+					uri: `https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}`,
 				  }}
 				/>
 			
@@ -88,7 +87,7 @@ class Quickcharts extends Component {
 				<Image
 				  style={styles.chart}
 				  source={{
-					uri: `${url}`,
+					uri: `https://quickchart.io/chart?c=${JSON.stringify(c)}`,
 				  }}
 				/>
 			
