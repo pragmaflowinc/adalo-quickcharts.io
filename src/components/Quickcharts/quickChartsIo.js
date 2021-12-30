33a891950f7d8f040eb6d852114e7b0636546b39
Mitch Schwartz <mitch@pragmaflow.com>
2021-12-30 18:15:56 -0500

    working now because we switched all double quotes for single

--- src/components/Quickcharts/quickChartsIo.js
+++ src/components/Quickcharts/quickChartsIo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet, Image, Text } from 'react-native'
 
 
 const styles = StyleSheet.create({
@@ -30,8 +30,8 @@ class Quickcharts extends Component {
 			return <View></View>
 		}
 				
-		let c = {type: "bar", data: {}}
-		c.data.labels = this.props.listItems.map(item => item["xValues"])
+		let c = {type: 'bar', data: {}}
+		c.data.labels = this.props.listItems.map(item => item.xValues)
 		const aggregatedData = this.props.listItems.reduce((acc, item) => {
 			
 		
@@ -48,32 +48,36 @@ class Quickcharts extends Component {
 			},{}
 		)
 		
-		console.log(aggregatedData)
+		//console.log(aggregatedData)
 
 
 		c.data.datasets = [{
 			label: this.props.dataSetName,
 			data: Object.keys(aggregatedData).map(key => aggregatedData[key])
 		}]
-		
-		if(this.props.onUrl){
-			this.props.onUrl(
-				this.props.editor 
-				
-				? `https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}`
 
-				: `https://quickchart.io/chart?c=${JSON.stringify(c)}`
-			)
-		}
+		console.log(c)
+		
+		// if(this.props.onUrl){
+		// 	this.props.onUrl(
+		// 		this.props.editor 
+				
+		// 		? `https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}`
+
+		// 		: `https://quickchart.io/chart?c=${JSON.stringify(c)}`
+		// 	)
+		// }
 
 		if(this.props.editor){
 			return(
 				<View style ={styles.wrapper}>
-			
+				<Text>demo chart</Text>
 				<Image
 				  style={styles.chart}
 				  source={{
-					uri: `https://quickchart.io/chart?c={type:'bar',data:{labels:['Q1','Q2','Q3','Q4'], datasets:[{label:'Users',data:[50,60,70,180]},{label:'Revenue',data:[100,200,300,400]}]}}`,
+					uri: `https://quickchart.io/chart?c={type:'bar',data:{labels:['2021-12-06T17:44:12Z','2021-12-06T17:44:20Z'],datasets:[{label:'Daily Orders',data:['2','2']}]}}`,
+
+					
 				  }}
 				/>
 			
@@ -83,11 +87,13 @@ class Quickcharts extends Component {
 
 		return (
 			<View style = {styles.wrapper}>
-			
+				<Text>prod chart</Text>
 				<Image
 				  style={styles.chart}
 				  source={{
-					uri: `https://quickchart.io/chart?c=${JSON.stringify(c)}`,
+					uri: `https://quickchart.io/chart?c={type:'${c.type}',data:{labels:[${c.data.labels.map(label => `'${label}'`).join(',')}],datasets:[${c.data.datasets.map(dataset => {
+						return `{label:'${dataset.label}',data:[${dataset.data.join(",")}]}`
+					}).join(',')}]}}`,
 				  }}
 				/>
 			
